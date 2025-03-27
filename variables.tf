e3535be060801bae5186ba85f982e6812697966f
Eliya-shlomo <eliyashlomo7@gmail.com>
2025-03-27 15:11:08 +0100

    updating .gitignore

--- variables.tf
+++ variables.tf
@@ -1,20 +1,18 @@
-variable "AWS_ACCESS_KEY" {
-    type = string
-    default = "AKIATZVXLVNCOHACXB64"
-}
-
-variable "AWS_SECRET_KEY" {
-  type = string
-}
-
 variable "AWS_REGION" {
   type = string
   default = "us-east-2"
 
-
+  ## To Valid the chosen region is under the right instructions
   validation {
     condition = contains(["es-east-2",lower(var.AWS_REGION)])
     error_message = "Unsupported AWS Region specified. Supported regions includes:${var.AWS_REGION}"
   }
 }
+
+
+
+
+
+
+
 

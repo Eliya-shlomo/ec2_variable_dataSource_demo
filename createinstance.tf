c05e792077cdc511f0d1de6b86bf1fdbc9adf19a
Eliya-shlomo <eliyashlomo7@gmail.com>
2025-03-27 14:45:09 +0100

    Remove .terraform from tracking

--- createinstance.tf
+++ createinstance.tf
@@ -1,4 +1,9 @@
 resource "aws_instance" "my_instance_example" {
+  count         = 3
   ami           = "ami-05803413c51f242b7"
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "demoinstance-${count.index}"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -10,31 +10,11 @@ variable "AWS_SECRET_KEY" {
 variable "AWS_REGION" {
   type = string
   default = "us-east-2"
+
+
+  validation {
+    condition = contains(["es-east-2",lower(var.AWS_REGION)])
+    error_message = "Unsupported AWS Region specified. Supported regions includes:${var.AWS_REGION}"
+  }
 }
 
-# variable "Security_Group"{
-#     type = list
-#     default = ["sg-24076", "sg-90890", "sg-456789"]
-# }
-
-# variable "AMIS" {
-#     type = map
-#     default = {
-#         us-east-1 = "ami-0f40c8f97004632f9"
-#         us-east-2 = "ami-05692172625678b4e"
-#         us-west-2 = "ami-0352d5a37fb4f603f"
-#         us-west-1 = "ami-0f40c8f97004632f9"
-#     }
-# }
-
-# variable "PATH_TO_PRIVATE_KEY" {
-#   default = "levelup_key"
-# }
-
-# variable "PATH_TO_PUBLIC_KEY" {
-#   default = "levelup_key.pub"
-# }
-
-# variable "INSTANCE_USERNAME" {
-#   default = "ubuntu"
-# }

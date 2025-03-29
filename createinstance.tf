84a73424e3d9df7f887f3ea5b945538660dada6f
Eliya-shlomo <eliyashlomo7@gmail.com>
2025-03-29 14:21:00 +0100

    FIXED ERROR! - SUCCSSES on deploying t2.micro using data source. the error Identified as observing the wrong place on aws console. (AMIs catalog instaed of: AMIs -> public images

--- createinstance.tf
+++ createinstance.tf
@@ -1,18 +1,31 @@
 data "aws_availability_zones" "available" {}
 
-data "aws_ami" "latest-ubuntu" {
+
+## finding the ami details on aws console - AMIs: choose public images - and than got the full details of the ami.
+data "aws_ami" "latest_ubuntu" {
   most_recent = true
-  owners = ["099720109477"]
+  owners      = ["099720109477"]
   filter {
-    name = "virtualization-type"
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
     values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
   }
 }
 
+
 resource "aws_instance" "my_instance_example" {
   ## region got's from aws_region variable on variable file.
-  ami           = data.aws_ami.latest-ubuntu.id
-  instance_type = "t3.micro"
+  ami           = data.aws_ami.latest_ubuntu.id
+  instance_type = "t2.micro"
   availability_zone = data.aws_availability_zones.available.names[0]
 
   tags = {
